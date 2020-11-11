3b540839bc41ad5f5211e9c60deff2dc91cd8675
Eduardo De La Cruz <gualdodelacruz@gmail.com>
2020-11-11 13:22:55 +0100

    Initial Commit

---
+++ Set Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set Game
+//
+//  Created by De La Cruz, Eduardo on 11/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by De La Cruz, Eduardo on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
